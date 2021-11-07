746d7db96cf83fbdbf7d8be80ca8865bd7ed681d
Marek Krchňavý <marek.krchnavy2@gmail.com>
2021-11-07 21:01:30 +0100

    feature(SB-85): Create recipe endpoint

--- src/endpoints/recipes.ts
+++ src/endpoints/recipes.ts
@@ -34,7 +34,47 @@ export const loadRecipe = (req: Request, res: Response) => {
   res.status(200).send('TODO: loadRecepie');
 };
 
-export const createRecipe = (req: Request, res: Response) => {
+export const createRecipe = async (req: Request, res: Response) => {
   console.log(req.body);
-  res.status(200).send('TODO: createRecepie');
+  const { name, description, locked } = req.body;
+  const instructions = req.body.Instructions.map((elem: any) => {
+    elem.Blocks = {
+      connectOrCreate: {
+        where: {
+          name: elem.Block,
+        },
+        create: {
+          name: elem.Block,
+        },
+      },
+    };
+
+    elem.Function_templates = { connect: { id: elem.function_template_id } }
+    delete elem.function_template_id
+    delete elem.Block;
+
+    if (elem.hasOwnProperty("function_option_id")) {
+      elem.Function_options = { connect: { id: elem.function_option_id } }
+      delete elem.function_option_id
+    }
+    return elem;
+  });
+
+  const result = await db.recipes.create({
+    data: {
+      name,
+      description,
+      locked,
+      Ingredients: {
+        createMany: {
+          data: req.body.Ingredients,
+        },
+      },
+      Instructions: {
+        create: instructions
+      },
+    },
+  });
+
+  res.json(result);
 };
