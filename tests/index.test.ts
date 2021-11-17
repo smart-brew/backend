f3d7aab17c988b45b6cf5567685f758ea9750f9b
Marek Vajda <marek.vajda009@gmail.com>
2021-11-17 18:15:02 +0100

    fix(SB-103): jest setup

---
+++ tests/index.test.ts
@@ -0,0 +1,8 @@
+// import supertest from 'supertest';
+// import app from '../src/index';
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true);
+  });
+});
