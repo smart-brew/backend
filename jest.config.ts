d404883b171e9f0e67d83f85074e57cc9a0a3368
Marek Vajda <marek.vajda009@gmail.com>
2021-12-06 10:28:13 +0100

    feature(SB-124): databaza rebase

--- jest.config.ts
+++ jest.config.ts
@@ -14,7 +14,7 @@ export default {
   // cacheDirectory: "C:\\Users\\marek\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
-  // clearMocks: false,
+  clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
